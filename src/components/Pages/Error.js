f4ac36ee350bf09e077e246b086a250390587b86
Debopriyo Basu <debopriyobasu@gmail.com>
2023-12-06 19:40:46 +0530

    Created Error component

--- src/components/Pages/Error.js
+++ src/components/Pages/Error.js
@@ -1,5 +1,18 @@
+import { MdError } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="h-screen w-full p-5 flex flex-col items-center bg-lemongreen justify-center">
+      <MdError size={100} className="fill-lemonred animate-pulse" />
+      <h1 className="text-karla text-4xl text-white text-center py-3">
+        The page you are looking for does not exist or has been moved.
+      </h1>
+      <Link to="/" className="btn py-3">
+        Go to Home
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
--- src/components/Pages/HomePage/Specials.js
+++ src/components/Pages/HomePage/Specials.js
@@ -4,7 +4,7 @@ import { menuList } from "../../../utils/constants";
 const Specials = () => {
   return (
     <div className="bg-white text-lemongreen px-3">
-      <div className="lg:container lg:mx-auto lg:max-w-screen-lg pt-5 md:pt-16">
+      <div className="lg:container lg:mx-auto lg:max-w-screen-lg pt-5 pb-5 md:pt-16">
         <div className="flex justify-between items-center">
           <h2 className="text-4xl font-extrabold">Specials</h2>
           <Link to={"/order-online"} className="btn">
