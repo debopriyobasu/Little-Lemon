41b21d28810bc72b3a5ca7639f5e467df8a58ecf
Debopriyo Basu <debopriyobasu@gmail.com>
2023-12-07 16:23:54 +0530

    Developed the booking confirmation page

--- src/components/Pages/Reservations/ConfirmedBooking.js
+++ src/components/Pages/Reservations/ConfirmedBooking.js
@@ -1,10 +1,14 @@
-function ConfirmedBooking() {
+import { GiConfirmed } from "react-icons/gi";
+
+const ConfirmedBooking = () => {
   return (
-    <div>
-      <h1>Booking Confirmed</h1>
-      <p>Your booking has been confirmed. Thank you!</p>
+    <div className="h-screen w-full p-5 flex flex-col items-center bg-lemongreen justify-center">
+      <GiConfirmed size={150} className="fill-lemonyellow" />
+      <h1 className="text-karla text-4xl text-white text-center">
+        Your booking has been confirmed. Thank you!
+      </h1>
     </div>
   );
-}
+};
 
 export default ConfirmedBooking;
