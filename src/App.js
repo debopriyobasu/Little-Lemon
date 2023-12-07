1484c319b9e6009e8db0427cf10038445c164008
Debopriyo Basu <debopriyobasu@gmail.com>
2023-12-07 16:28:48 +0530

    Changed default SPA behaviour on route change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <>
       <Header />
